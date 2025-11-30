7db8fe7ba3fa01a35e34df8f0598cd7b4d5d36a9
Kuya KOHARA <kuya.kohara@gmail.com>
2025-12-01 01:13:08 +0900

    feat: add read_to_string

--- crates/wasm-actions-core/src/fs.rs
+++ crates/wasm-actions-core/src/fs.rs
@@ -2,3 +2,12 @@ mod file;
 
 pub use file::File;
 pub use file::OpenOptions;
+use crate::io::AsyncReadExt;
+
+pub async fn read_to_string<P: AsRef<str>>(path: P) -> std::io::Result<String> {
+    let path = path.as_ref().to_owned();
+    let mut f = File::open(&path).await?;
+    let mut s = String::new();
+    f.read_to_string(&mut s).await?;
+    Ok(s)
+}
