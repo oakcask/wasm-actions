9e94ea23ae1ef07bcf532dc6a117c636932c770a
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-30 16:21:57 +0000

    style: `cargo --locked fmt -p wasm-actions-prelude`
    https://github.com/oakcask/wasm-actions/actions/runs/19801636823

--- crates/wasm-actions-prelude/src/output.rs
+++ crates/wasm-actions-prelude/src/output.rs
@@ -1,8 +1,4 @@
-
-use wasm_actions_core::{
-    error::Error,
-    fs::OpenOptions, io::AsyncWriteExt as _,
-};
+use wasm_actions_core::{error::Error, fs::OpenOptions, io::AsyncWriteExt as _};
 
 use crate::env;
 
@@ -11,7 +7,11 @@ pub async fn set_output(name: &str, value: &str) -> Result<(), Error> {
     let mut buf = Vec::<u8>::new();
     writeln!(&mut buf, "{}={}", name, value).map_err(Error::new)?;
     let path = env::var("GITHUB_OUTPUT").ok_or_else(|| Error::from("$GITHUB_OUTPUT unset"))?;
-    let mut f = OpenOptions::new().append(true).open(path).await.map_err(Error::new)?;
+    let mut f = OpenOptions::new()
+        .append(true)
+        .open(path)
+        .await
+        .map_err(Error::new)?;
     f.write_all(&buf).await.map_err(Error::new)?;
     f.shutdown().await.map_err(Error::new)?;
     Ok(())
@@ -22,7 +22,11 @@ pub async fn save_state(name: &str, value: &str) -> Result<(), Error> {
     let mut buf = Vec::<u8>::new();
     writeln!(&mut buf, "{}={}", name, value).map_err(Error::new)?;
     let path = env::var("GITHUB_STATE").ok_or_else(|| Error::from("$GITHUB_STATE unset"))?;
-    let mut f = OpenOptions::new().append(true).open(path).await.map_err(Error::new)?;
+    let mut f = OpenOptions::new()
+        .append(true)
+        .open(path)
+        .await
+        .map_err(Error::new)?;
     f.write_all(&buf).await.map_err(Error::new)?;
     f.shutdown().await.map_err(Error::new)?;
     Ok(())
