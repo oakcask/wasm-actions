e05023a92fed57db33f2bcf3356354e230ae9871
Kuya KOHARA <kuya.kohara@gmail.com>
2025-12-01 02:54:50 +0900

    chore: fix deps

--- crates/wasm-actions-derive/src/codegen.rs
+++ crates/wasm-actions-derive/src/codegen.rs
@@ -5,8 +5,15 @@ use wasm_actions_parse::{InputAttr, InputSource, OutputAttr, OutputName};
 
 pub(crate) fn start_fn(input: DeriveInput) -> Result<TokenStream, Error> {
     let ident = input.ident;
+    // HACK: using #[wasm_bindgen] via wasm_bindgen_futures will generate
+    // error complain about unlinked crate named `wasm_bindgen_futures`.
+    // This provide small help for users to know that wasm-bindgen-futures
+    // is needed as dependency.
+    // But... it is painful so we might have to return a Promise directly.
+    //
+    // see wasm-actions.
     Ok(quote! {
-        #[wasm_bindgen::prelude::wasm_bindgen]
+        #[wasm_actions::derive::wasm_bindgen]
         pub async fn start() -> Result<(), wasm_bindgen::prelude::JsError> {
             use wasm_actions::derive::Action;
             let input = #ident::parse_input()?;
