3f4deaa6247a78cebabaf860a650f51f0e73f096
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-30 16:38:12 +0000

    style: `cargo --locked fmt -p wasm-actions-core`
    https://github.com/oakcask/wasm-actions/actions/runs/19801818137

--- crates/wasm-actions-core/src/fs.rs
+++ crates/wasm-actions-core/src/fs.rs
@@ -1,8 +1,8 @@
 mod file;
 
+use crate::io::AsyncReadExt;
 pub use file::File;
 pub use file::OpenOptions;
-use crate::io::AsyncReadExt;
 
 pub async fn read_to_string<P: AsRef<str>>(path: P) -> std::io::Result<String> {
     let path = path.as_ref().to_owned();
