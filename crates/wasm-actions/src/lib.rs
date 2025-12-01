33fd9bfee1f0dd50ffa8b8d9471ac822c771dcab
Kuya KOHARA <kuya.kohara@gmail.com>
2025-12-01 17:52:02 +0900

    chore: fix deps (#88)
    * chore: fix deps
    
    * style: `cargo --locked fmt -p wasm-actions`
    
    https://github.com/oakcask/wasm-actions/actions/runs/19816686558
    
    ---------
    
    Co-authored-by: update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>

--- crates/wasm-actions/src/lib.rs
+++ crates/wasm-actions/src/lib.rs
@@ -4,8 +4,15 @@ pub mod prelude {
 
 #[cfg(feature = "derive")]
 pub mod derive {
+    // HACK: using #[wasm_bindgen] via wasm_bindgen_futures will generate
+    // error complain about unlinked crate named `wasm_bindgen_futures`.
+    // This provide small help for users to know that wasm-bindgen-futures
+    // is needed as dependency.
+    // But... it is painful so we might have to return a Promise directly.
     pub use wasm_actions_derive::*;
     pub use wasm_actions_prelude::derive::*;
+    #[doc(hidden)]
+    pub use wasm_bindgen_futures::wasm_bindgen::prelude::wasm_bindgen;
 }
 
 #[cfg(feature = "testing")]
