157efbd7b5828f8b504d216df1d69b57f650ad6f
Kuya KOHARA <kuya.kohara@gmail.com>
2025-12-02 00:23:41 +0900

    chore: run trybuild tests (uh-oh that's never run)

--- crates/wasm-actions-macro/ui-tests/pass/input_var_underscore.rs
+++ crates/wasm-actions-macro/ui-tests/pass/input_var_underscore.rs
@@ -1,4 +1,4 @@
-use actions_rs_macro::input_var_underscore;
+use wasm_actions_macro::input_var_underscore;
 
 const EXPECTED: &str = "INPUT_FOO_BAR_BAZ_";
 const GOT: &str = input_var_underscore!("Foo bar-baZ ");
